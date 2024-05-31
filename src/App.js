50cccd09ebd963f07c17ab9387a0298bfc81928a
Roshis <roshis.tamang@gmail.com>
2024-05-31 14:38:10 +1000

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Box, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import NavBar from "./components/NavBar";
+import Sidebar from "./components/SideBar";
+import MovieContent from "./components/MovieContent";
+import "./components/main.css";
+const theme = createTheme({});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <div className="parent">
+        <div className="nav">
+          <NavBar />
+        </div>
+
+        <Box
+          sx={{
+            display: "flex",
+
+            justifyContent: "space-between",
+            padding: "55px",
+            position: "relative",
+            // marginTop: "20px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="sidebar-main">
+            <Sidebar className="sidebar" />
+          </div>
+          <div className="movie-content">
+            <MovieContent />
+          </div>
+        </Box>
+      </div>
+    </ThemeProvider>
   );
 }
 
